f441b0beb2f3cdfde4d5a519de55312dcfb30c10
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-02 19:46:01 -0500

    Time: 172 ms (73.22%), Space: 24.1 MB (70.82%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        sort(num.begin(), num.end());
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // Sort the input array.
+        sort(nums.begin(), nums.end());  
         vector<vector<int>> ans;
-        for(int i = 0; i < num.size(); i++) {
-            if (i > 0 && num[i] == num[i - 1])
+        for (int i = 0; i < nums.size(); i++) {
+            // Ignore the same number to avoid duplicate triplets.
+            if (i > 0 && nums[i] == nums[i-1]) 
                 continue;
-            int j = i + 1, k = num.size() - 1;
+            int j = i + 1, k = nums.size() - 1;
             while (j < k) {
-                int sum = num[i] + num[j] + num[k];
+                int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
-                    ans.push_back({num[i], num[j], num[k]});
-                    while (j < k && num[j] == num[j + 1])
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    // Skip the same numbers from j to avoid duplicate triplets.
+                    while (j < k && nums[j] == nums[j+1]) 
                         j++;
-                    while (j < k && num[k] == num[k - 1])
+                    // Skip the same numbers from k to avoid duplicate triplets.
+                    while (j < k && nums[k] == nums[k-1])  
                         k--;
-                    j++;
-                    k--;    
-                }
+                    // Move both pointers inward.
+                    j++; k--;
+                } 
+                // If the sum < 0, move j to the right.
                 else if (sum < 0) 
                     j++;
+                // If the sum > 0, decrease k to the left.
                 else
                     k--;
             }
         }
-    return ans;
+        // Return the result vector.
+        return ans;
     }
 };
