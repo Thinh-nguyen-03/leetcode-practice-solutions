27ce49328e63ca91cd24a1b3171921248b0602e7
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-03 10:26:26 -0500

    Time: 51 ms (6.28%), Space: 23.1 MB (5.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-       int sum=0,n=nums.size();
-        for(int i=0;i<n;i++)
-        sum+=nums[i];
-        return ((n*(n+1))/2)-sum;
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int expectedNumCount = nums.size() + 1;
+        for (int number = 0; number < expectedNumCount; number++) {
+            if (numSet.find(number) == numSet.end()) {
+                return number;
+            }
+        }
+        return -1;
     }
 };
