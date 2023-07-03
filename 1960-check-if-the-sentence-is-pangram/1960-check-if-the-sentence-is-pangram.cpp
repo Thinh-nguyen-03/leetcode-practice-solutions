58a01bc00d4a709cfc460eda938714701ba217d0
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-03 10:11:05 -0500

    Time: 0 ms (100%), Space: 6.3 MB (100%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+        array<bool, 26> seen{};
+        for (auto currChar : sentence)
+            seen[currChar - 'a'] = true;
+        for (auto status : seen)
+            if (!status)
+                return false;
+        return true;
+
+        /* Using set
         unordered_set<char> seen(sentence.begin(), sentence.end());
-        return seen.size() == 26;
+        return seen.size() == 26; */
     }
 };
