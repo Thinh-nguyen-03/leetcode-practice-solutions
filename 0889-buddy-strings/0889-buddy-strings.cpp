f4061564e6afba1875b18e54373b7e9476bb3691
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-02 21:31:12 -0500

    Time: 6 ms (44.19%), Space: 6.8 MB (99.57%) - LeetHub

--- 0889-buddy-strings/0889-buddy-strings.cpp
+++ 0889-buddy-strings/0889-buddy-strings.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
-        if (s.size() != goal.size()) {
+        if (s.size() != goal.size()) 
             return false;
-        }
         if (s == goal) {
             int letters[26] = {0};
             for (char c : s) {
-                if (++letters[c - 'a'] > 1) {
+                int swappable = ++letters[c - 'a'];
+                if (swappable > 1)
                     return true;
-                }
             }
             return false;
-        } else {
+        } 
+        else {
             int count = 0;
             pair<int, int> diff;
             for (int i = 0; i < s.size(); ++i) {
                 if (s[i] != goal[i]) {
                     ++count;
-                    if (count > 2) {
+                    if (count > 2) 
                         return false;
-                    }
-                    if (count == 1) {
+                    if (count == 1) 
                         diff.first = i;
-                    } else {
+                    else 
                         diff.second = i;
-                    }
                 }
             }
             return count == 2 && s[diff.first] == goal[diff.second] && s[diff.second] == goal[diff.first];
