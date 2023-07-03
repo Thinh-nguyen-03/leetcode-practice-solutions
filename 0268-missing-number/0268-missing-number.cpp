26fcaa615054fcffa91a868d8054f1fb515a3efa
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-03 10:24:43 -0500

    Time: 23 ms (51.96%), Space: 17.9 MB (89.48%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int total = n * (n + 1) / 2; // Gauss' formula
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
-        return total - sum; // The missing number is the difference
+        return n * (n + 1) / 2 - sum; // Gauss' formula, the missing number is the difference
     }
 };
