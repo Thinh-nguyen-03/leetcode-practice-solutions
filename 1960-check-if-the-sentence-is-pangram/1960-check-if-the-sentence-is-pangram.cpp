22d33b2202206f4fe24741c2d138f4e59f985b16
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-03 10:10:29 -0500

    Time: 0 ms (100%), Space: 7.2 MB (6.88%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        array<bool, 26> seen{};
-        for (auto currChar : sentence)
-            seen[currChar - 'a'] = true;
-        for (auto status : seen)
-            if (!status)
-                return false;
-        return true;
+        unordered_set<char> seen(sentence.begin(), sentence.end());
+        return seen.size() == 26;
     }
 };
