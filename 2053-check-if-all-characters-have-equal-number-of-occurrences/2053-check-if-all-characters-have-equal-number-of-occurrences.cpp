7500ad092a67febd79d68429ae4a55c2728f714a
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-04 04:20:05 -0500

    Time: 4 ms (74.01%), Space: 6.8 MB (57.09%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool areOccurrencesEqual(string s) {
-        unordered_map<char,int> freq;
-        for(auto i : s)
-            freq[i]++;
-        int val = freq[s[0]];
-        for(auto i : freq)
-            if(i.second != val)
-                return false;
-        return true;
+        unordered_map<char, int> freq;  
+        for (auto i : s) 
+            freq[i]++;  // Increment the frequency of the current character in the map.
+        int val = freq[s[0]];  
+        for (auto i : freq)  
+            // If the frequency of any character != the frequency of the 1st character
+            if (i.second != val)  
+                return false;  
+        return true;  
     }
 };
