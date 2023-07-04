2d50d6c547aef5a3c5291301c8ae236f825844af
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2023-07-04 04:46:04 -0500

    Time: 131 ms (91.81%), Space: 69.6 MB (45.69%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        bool flag = false;
-        for(int i =0;i<int(nums.size()-1);i++){
-            if(nums[i] == nums[i+1]) return true;
+        unordered_set<int> uniqueNums;
+        for (int num : nums) {
+            if (uniqueNums.count(num) > 0) {
+                return true;
+            }
+            uniqueNums.insert(num);
         }
-        return flag;
+        return false;
     }
 };
