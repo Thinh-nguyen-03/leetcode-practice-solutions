80128d6803f476f22e4f38295ddec5c5f55aa417
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2024-07-03 08:48:04 -0500

    Time: 17 ms (24.44%), Space: 11.7 MB (21.15%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -20,6 +20,8 @@ class MyStack(object):
         """
         :rtype: int
         """
+        if not self.queue1:
+            return -1
         return self.queue1.popleft()
 
     def top(self):
