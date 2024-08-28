817d931dc9cb1cfe900f70f5a39604ed1854329b
Thinh-nguyen-03 <136863889+Thinh-nguyen-03@users.noreply.github.com>
2024-08-28 02:52:47 -0500

    Time: 549 ms (77.56%) | Memory: 78.3 MB (47.75%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,38 +1,55 @@
-from collections import OrderedDict
+class Node:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
-class LRUCache(object):
+class LRUCache:
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
-        self.cache = OrderedDict()  # Initialize an empty OrderedDict
-        self.capacity = capacity    # Store the capacity of the cache
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # Hash map to store key -> Node
+        self.head = Node()  # Dummy head
+        self.tail = Node()  # Dummy tail
+        self.head.next = self.tail  # Connect head to tail
+        self.tail.prev = self.head  # Connect tail to head
 
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
+    def _remove(self, node):
+        # Remove a node from the linked list
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add_to_front(self, node):
+        # Add a new node right after the head (front of the list)
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
         if key in self.cache:
-            # Manually move the accessed key to the end to mark it as most recently used
-            value = self.cache.pop(key)
-            self.cache[key] = value  # Reinsert the key to move it to the end
-            return value
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value  # Update the value
+            self._remove(node)
+            self._add_to_front(node)
         else:
-            return -1
-
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        if key in self.cache:
-            # Remove the existing entry
-            self.cache.pop(key)
-        self.cache[key] = value  # Insert the key-value pair
-
-        # If the cache exceeds its capacity, pop the first item (least recently used)
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)
+            if len(self.cache) == self.capacity:
+                # Remove the least recently used node (from the tail)
+                lru_node = self.tail.prev
+                self._remove(lru_node)
+                del self.cache[lru_node.key]
+            # Add the new node
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_front(new_node)
